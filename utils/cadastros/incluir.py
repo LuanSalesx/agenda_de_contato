c0a9ea5344f68d3ab7dbc2920261b14b361b146b
Luan Dev <luansales.oficial@gmail.com>
2024-09-30 08:43:09 -0300

    Adicionado o código em incluir.py para a inicialização do projeto.

--- utils/cadastros/incluir.py
+++ utils/cadastros/incluir.py
@@ -1,1 +1,23 @@
 # Incluir contatos
+import os
+def incluir(agenda_file, nome, email, telefone):    
+    nome = nome.upper()
+    email = email.upper()
+    telefone = telefone.upper()
+
+    if not os.path.exists(agenda_file):
+        open(agenda_file, 'w').close()
+
+    
+    with open(agenda_file, 'a') as arquivo:
+        linhas = arquivo.readlines()
+        for linha in linhas:
+            contato_data = linha.strip().split(',')
+            if contato_data[0] == nome or contato_data[1] == email:
+                print(f"ERRO: Contato {nome} já existe na agenda.")
+                return
+            
+    id_conato = len(linhas) + 1
+    with open(agenda_file, 'a') as arquivo:
+        arquivo.write(f"{id_conato}, {nome}, {email}, {telefone}\n")
+        print(f"Contato {nome} incluído com sucesso.")
