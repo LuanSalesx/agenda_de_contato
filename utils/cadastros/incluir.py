b4872fac9ad3c98e2748c93de760a0266b0be914
Luan Dev <luansales.oficial@gmail.com>
2024-09-30 16:10:54 -0300

    Update

--- utils/cadastros/incluir.py
+++ utils/cadastros/incluir.py
@@ -1,23 +1,22 @@
-# Incluir contatos
 import os
-def incluir_contato(agenda_file, nome, email, telefone):    
+
+def incluir_contato(agenda_path, nome, email, telefone):
     nome = nome.upper()
     email = email.upper()
     telefone = telefone.upper()
 
-    if not os.path.exists(agenda_file):
-        open(agenda_file, 'w').close()
+    if not os.path.exists(agenda_path):
+        open(agenda_path, 'w').close()
 
-    
-    with open(agenda_file, 'a') as arquivo:
-        linhas = arquivo.readlines()
-        for linha in linhas:
-            contato_data = linha.strip().split(',')
-            if contato_data[0] == nome or contato_data[1] == email:
-                print(f"ERRO: Contato {nome} já existe na agenda.")
+    with open(agenda_path, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            contact_data = line.strip().split(',')
+            if contact_data[1] == nome or contact_data[2] == email:
+                print("Erro: Nome ou email já cadastrado!")
                 return
-            
-    id_contato = len(linhas) + 1
-    with open(agenda_file, 'a') as arquivo:
-        arquivo.write(f"{id_contato}, {nome}, {email}, {telefone}\n")
-        print(f"Contato {nome} incluído com sucesso.")
+
+    id_contato = len(lines) + 1  # ID será sempre crescente
+    with open(agenda_path, 'a') as file:
+        file.write(f"{id_contato},{nome},{email},{telefone}\n")
+    print("Contato incluído com sucesso!")
