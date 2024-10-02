4f75d9cb3f30fdac18f426b3a2ab02423747dec6
Luan Dev <luansales.oficial@gmail.com>
2024-10-02 07:49:25 -0300

    Added em pesquisar.py

--- utils/cadastros/pesquisar.py
+++ utils/cadastros/pesquisar.py
@@ -1,2 +1,13 @@
+# Pesquisar contato
 def pesquisar_contato(id):
-    print(id)
+    try:
+        with open("agenda.txt", "r") as agenda:
+            contatos = agenda.readlines()
+            # Pesquisa o ctt pelo ID
+            for contato in contatos:
+                id_contato, nome, email, telefone = contato.strip().split(";")
+                if int(id_contato) == id:
+                    return f"ID: {id_contato}, Nome: {nome}, Email: {email}, Telefone: {telefone}"
+                return "Contato não encontrado."
+    except FileNotFoundError:
+        return "Arquivo não encontrado. Crie um contato primeiro"
